4a3ee20a24ee043ea391a906f8757584cb323270
ZloyLisi <github.2005.18@gmail.com>
2023-09-19 22:54:29 +0300

    comm2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,3 @@
-   //задание1
-fun  printFullName(firstName: String, lastName: String)
-{
-    println("$firstName $lastName")
-}
-
 
 fun main(args: Array<String>)
 {
@@ -19,16 +13,47 @@ fun main(args: Array<String>)
     //задание4
     println(calculateFullName2("Паразихин","Александр").second)
 
+    //задание5
+    val primeNumber = primeNumber( 29)
+    println(primeNumber)
+
+    //задание6
+    val fib = fibonacci(9)
+    println(fib)
+
 }
-    //задание3
+//задание1
+fun  printFullName(firstName: String, lastName: String)
+{
+    println("$firstName $lastName")
+}
+
+
+//задание3
 fun calculateFullName(firstName: String,lastName: String) = firstName + lastName
 
 
    //задание4
-   fun calculateFullName2(firstName: String,lastName: String):Pair<String,Int> {
-       return Pair("$firstName $lastName", firstName.length + lastName.length)
-   }
+fun calculateFullName2(firstName: String,lastName: String):Pair<String,Int>
+{
+   return Pair("$firstName $lastName", firstName.length + lastName.length)
+}
 
-
+//задание 5
+fun primeNumber (number: Int) : Boolean
+{
+    for (i in 2 .. (number/2))
+    {
+        if (number % i == 0)
+            return false
+    }
+    return true
+}
+//задание6
+fun fibonacci(n: Int): Int
+{
+    if (n == 1 || n == 2) return 1
+    else return fibonacci(n - 1) + fibonacci(n - 2)
+}
 
 
